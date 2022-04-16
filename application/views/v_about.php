a1fc1172d897a205a5907a75133aeecea2e65b5b
andre wardana <nadhiralbabm@gmail.com>
2022-04-16 21:05:17 +0700

    mengumpulkan tugas 5

--- application/views/v_about.php
+++ application/views/v_about.php
@@ -2,13 +2,13 @@
 <h1><?php echo $judul ?></h1>
 <h4>Nama</h4>     
 <ul type="disc">         
-<li>Nama Depan : Nadhir Albab</li>         
-<li>Nama Belakang : Musthofa</li>     
+<li>Nama Depan : Andre</li>         
+<li>Nama Belakang : Wardana</li>     
 </ul>     
 <br>     
 <h4>Alamat</h4>     
 <ul type="none">         
-<li> Jalan Raya Babakan, Perumahan Taman Semesta Mas, Blok K no.7 Tajurhalang-Bogor</li>     
+<li> Kukusan, Beji, Depok</li>     
 </ul>          
 <h4>Tempat Lahir</h4>     
 <ul type="none">         
@@ -17,7 +17,7 @@
  
     <h4>Olah Raga Favorit</h4>     
 <ul type="square">         
-<li>Bulutangkis</li>         
-<li>E-sport</li>     
+<li>Taekwondo</li>         
+<li>Catur</li>     
 </ul> 
 </section>  
